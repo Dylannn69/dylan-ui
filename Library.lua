16c3495c82c4cb6419340f46d8ce84883822fe87
Dylannn69 <markdaylan07@gmail.com>
2025-11-04 12:05:29 +0800

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -140,7 +140,7 @@ Toggle.BackgroundColor3 = Color3.new(1, 1, 1)
 Toggle.BackgroundTransparency = 1
 Toggle.Position = UDim2.new(0, 5, 0, -2)
 Toggle.Rotation = 90
-Toggle.Size = UDim2.new(0, 25, 0, 25)
+Toggle.Size = UDim2.new(0, 23, 0, 23)
 Toggle.ZIndex = 2
 Toggle.Image = "https://www.roblox.com/Thumbs/Asset.ashx?width=420&height=420&assetId=113822902692512"
 
